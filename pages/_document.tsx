0ed510c1851e4556de0812c677937d3660cf5ae0
Maher E. Elmashharawi <mashmaher96@gmail.com>
2023-02-06 20:23:00 +0200

    favico path

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,9 +10,8 @@ export default class MyDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
-          <link rel="shortcut icon" href="../public/favicon.ico" />
+          <link rel="shortcut icon" href="./public/favicon.ico" />
           <meta name="emotion-insertion-point" content="" />
-
           {(this.props as any).emotionStyleTags}
         </Head>
         <body>
